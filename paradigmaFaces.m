4c856daacd2b5744bab74e5bba21314d15cfafb8
joborch <s1078397@stud.plus.ac.at>
2025-01-09 10:22:15 +0100

    Identation correction
    Korrigierte Einrückung :(

--- paradigmaFaces.m
+++ paradigmaFaces.m
@@ -7,14 +7,14 @@ cd(currentFolderPath); %Aktuelles Working Directory setzen!
 
 myScreen = 1; %Define Screen
 
-    white  = WhiteIndex(myScreen); %Color Index White
-    black = BlackIndex(myScreen); %Color Index Black
-    gray = (white+black)/2; %Color Index Gray
+white  = WhiteIndex(myScreen); %Color Index White
+black = BlackIndex(myScreen); %Color Index Black
+gray = (white+black)/2; %Color Index Gray
 
 color = white; %Definition of Color for myWindow
 
-    [width, height] = Screen('WindowSize', myScreen); %Reads indivdual Screen Size
-    ratioFactor = 0.75; %Factor for Screen: 1 is Fullscreen
+[width, height] = Screen('WindowSize', myScreen); %Reads indivdual Screen Size
+ratioFactor = 0.75; %Factor for Screen: 1 is Fullscreen
 
 ratio = [0 0 width*ratioFactor height*ratioFactor]; %Definition of Ratio for myWindow
 
